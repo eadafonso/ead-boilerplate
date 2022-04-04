ce4d5e5dad2c375f1fce313406b6c9efc0aaeff3
Edvaldo Afonso <eadafonso@gmail.com>
2022-04-04 16:12:55 +0100

    feat: create add theme type

---
+++ src/styles/theme.ts
@@ -0,0 +1,34 @@
+export default {
+  border: {
+    radius: "0.4rem",
+  },
+  font: {
+    family: "Poppins, sans-serif",
+    title: "Poppins",
+    bold: 500,
+    light: 200,
+  },
+
+  sizes: {
+    xsmal: "0.5rem",
+    small: "1rem",
+    medium: "1.5rem",
+    large: "2rem",
+    xlarge: "2.5rem",
+    xxlarge: "3rem",
+  },
+
+  colors: {
+    primary: "#1c1c1c",
+    secondary: "#141414",
+  },
+
+  spacings: {
+    xsmal: "0.5rem",
+    small: "1rem",
+    medium: "1.5rem",
+    large: "2rem",
+    xlarge: "2.5rem",
+    xxlarge: "3rem",
+  },
+};
